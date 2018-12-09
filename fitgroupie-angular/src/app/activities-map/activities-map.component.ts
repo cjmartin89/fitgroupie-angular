99d5faf07f175295c4f606ab5b9ab39e98321f1b
Chris Martin <martintechsolution@gmail.com>
2018-12-08 23:00:11 -0500

    Initial Commit Setup

---
+++ fitgroupie-angular/src/app/activities-map/activities-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-map',
+  templateUrl: './activities-map.component.html',
+  styleUrls: ['./activities-map.component.css']
+})
+export class ActivitiesMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- fitgroupie-angular/src/app/app.module.ts
+++ fitgroupie-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ActivitiesMapComponent } from './activities-map/activities-map.component';
+import { ActivitiesListComponent } from './activities-list/activities-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ActivitiesMapComponent,
+    ActivitiesListComponent
   ],
   imports: [
     BrowserModule
